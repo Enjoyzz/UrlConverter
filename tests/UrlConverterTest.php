ba1c15ac02ad57d47c8a060fc72a3eab2d05f499
Enjoys <deadly.info@gmail.com>
2021-08-02 12:59:45 +0300

    added new tests

--- tests/UrlConverterTest.php
+++ tests/UrlConverterTest.php
@@ -14,19 +14,20 @@ class UrlConverterTest extends TestCase
             ['http://yandex.ru/test.css', '1/2/3/', 'http://yandex.ru/1/2/3/'],
             ['http://yandex.ru/test.css', '../3/', 'http://yandex.ru/3/'],
             ['http://yandex.ru/test.css', '../../../../3/', 'http://yandex.ru/3/'],
+            ['http://yandex.ru/1/2/3/4/5/6/7/8/9/test.css', '../../../../img/logo.png', 'http://yandex.ru/1/2/3/4/5/img/logo.png'],
             ['http://yandex.ru/1/2/3/test.css', '../4/', 'http://yandex.ru/1/2/4/'],
             ['http://yandex.ru/1/2/3/test.css', './4/', 'http://yandex.ru/1/2/3/4/'],
             ['http://yandex.ru/1/2/3/test.css', '../../4/', 'http://yandex.ru/1/4/'],
             ['http://yandex.ru/1/2/3/test.css', '/4', 'http://yandex.ru/4'],
             [
                 'http://usr:pss@example.com:81/mypath/myfile.html?a=b&b[]=2&b[]=3#myfragment',
-                '/4',
-                'http://usr:pss@example.com:81/4?a=b&b[]=2&b[]=3#myfragment'
+                '/logo.jpg',
+                'http://usr:pss@example.com:81/logo.jpg?a=b&b[]=2&b[]=3#myfragment'
             ],
             [
                 'http://usr:pss@example.com:81/mypath/myfile.html?a=b&b[]=2&b[]=3#myfragment',
-                '4.txt',
-                'http://usr:pss@example.com:81/mypath/4.txt?a=b&b[]=2&b[]=3#myfragment'
+                'txt/4.txt',
+                'http://usr:pss@example.com:81/mypath/txt/4.txt?a=b&b[]=2&b[]=3#myfragment'
             ],
             [
                 'http:///fail',
